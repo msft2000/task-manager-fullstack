cc0755f2bdbd6967bd10b2ea9d5825ea250a1592
Mateo Flores <dragonnegro1231@gmail.com>
2023-02-16 12:25:45 -0500

    App lista para despliegue

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+const connectDB = require('./dataBase/conect');
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+require('dotenv').config();
+const port = process.env.PORT || 3000;
+ //middleware
+app.use(express.static('./public'));
+app.use(express.json());
+//routes
+app.use('/api/v1/tasks', tasks);
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, console.log(`server is listening on port ${port}...`));
+    } catch (error) {
+        console.log(error);
+    }
+}
+start()
