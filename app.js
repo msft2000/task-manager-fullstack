652d9c18c954c212f71e36c23ec6fd9b0cc149cc
Mateo Flores <dragonnegro1231@gmail.com>
2023-02-16 12:51:26 -0500

    Cambios para dejar app funcional solo en local

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const serverless = require('serverless-http');
-const bodyParser = require('body-parser');
 const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./dataBase/conect');
@@ -16,17 +14,13 @@ app.use('/api/v1/tasks', tasks);
 app.use(notFound);
 app.use(errorHandlerMiddleware);
 
-// const start = async () => {
-//     try {
-//         await connectDB(process.env.MONGO_URI);
-//         app.listen(port, console.log(`server is listening on port ${port}...`));
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-// start()
-app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-module.exports = app;
-module.exports.handler = serverless(app);
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, console.log(`server is listening on port ${port}...`));
+    } catch (error) {
+        console.log(error);
+    }
+}
+start()
 
